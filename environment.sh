f8898afa6f2aad304a5d62a99ab21dae8518951c
Michael Diamond <diamondm@google.com>
2020-04-17 00:40:44 -0700

    Use bat for man pages if installed.

--- environment.sh
+++ environment.sh
@@ -2,5 +2,11 @@
 #
 # Update, specify, and compute environment variables, such as
 # PATH, or PS1 here.
-# 
+#
 
+# Environment customizations for users of https://github.com/sharkdp/bat
+if which bat >/dev/null; then
+  export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+fi
+
+pg::require bat "Installation instructions: https://github.com/sharkdp/bat"
