3744428926fc29416d7481732de1f188a5bf8bd8
Michael Diamond <dimo414@gmail.com>
2020-04-21 01:16:19 -0700

    Undo erroneous tweak in ffaa526b

--- environment.sh
+++ environment.sh
@@ -5,7 +5,7 @@
 #
 
 # Environment customizations for users of https://github.com/sharkdp/bat
-if which bat 2>/dev/null; then
+if which bat >/dev/null; then
   export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 fi
 
